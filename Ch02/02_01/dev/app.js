d1394e6f1f4e00b32ea2d7c19f4521c7dc34ac8d
John <john@real-currents.com>
2020-02-21 14:49:16 -0500

    start dev

---
+++ Ch02/02_01/dev/app.js
@@ -0,0 +1,12 @@
+(function() {
+    "use strict";
+
+    const ProductCustomizer = React.createElement(
+        "div",
+        { className: "customizer" },
+        "Product Customizer Content"
+    );
+
+    ReactDOM.render(ProductCustomizer, document.getElementById("react-root"));
+
+})();
