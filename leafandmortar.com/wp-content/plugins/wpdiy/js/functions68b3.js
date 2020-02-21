296393e2bd51c9a119aa433f1463c17340fe0514
John Hall <john@real-currents.com>
2020-02-20 21:50:11 -0500

    clean up

--- leafandmortar.com/wp-content/plugins/wpdiy/js/functions68b3.js
+++
@@ -1,7 +0,0 @@
-// Toggle info section on and off
-jQuery(document).ready(function($){
-    $("a.info-trigger").click(function(){
-        $(".super-footer").slideToggle('slow');
-        return false;
-    });
-});
